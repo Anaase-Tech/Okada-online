af550fd9010c607f286d601de0de4014fccdb8c5
Berima1 <Eddy199099@gmail.com>
2026-04-13 09:43:41 +0000

    Update firebase.js

--- web-app/src/firebase.js
+++ web-app/src/firebase.js
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey:            process.env.REACT_APP_FIREBASE_API_KEY || "demo",
-  authDomain:        "okada-online-ghana.firebaseapp.com",
-  projectId:         "okada-online-ghana",
-  storageBucket:     "okada-online-ghana.appspot.com",
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "000",
-  appId:             process.env.REACT_APP_FIREBASE_APP_ID || "demo",
+  apiKey:            process.env.REACT_APP_FIREBASE_API_KEY            || "demo",
+  authDomain:        process.env.REACT_APP_FIREBASE_AUTH_DOMAIN        || "okada-online-ghana.firebaseapp.com",
+  projectId:         process.env.REACT_APP_FIREBASE_PROJECT_ID         || "okada-online-ghana",
+  storageBucket:     process.env.REACT_APP_FIREBASE_STORAGE_BUCKET     || "okada-online-ghana.appspot.com",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID|| "000",
+  appId:             process.env.REACT_APP_FIREBASE_APP_ID             || "demo",
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db   = getFirestore(app);
 export default app;
